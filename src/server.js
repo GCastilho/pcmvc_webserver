79b02612d3e84f2ebecda2a706ef05e8646b4f20
Gabriel Castilho <castilho.gabriel14@gmail.com>
2019-08-07 12:03:22 -0300

    Adicionado home page
    -- Adicionado a pasta 'public' para htmls estáticos
    -- Adicionado home page com form para colher os dados de telemetria
    -- Adicionado handler default para 404

--- src/server.js
+++ src/server.js
@@ -1,7 +1,14 @@
+const path = require('path')
 const express = require('express')
 
 const app = express()
 const port = process.env.PORT || 3000
+
+/**@description Define paths for express configs */
+const publicDirectoryPath = path.join(__dirname, '../public');
+
+/**@description Setup static directory to serve */
+app.use(express.static(publicDirectoryPath));
 
 /**
  * @description Os handlers para todos os requests para a API
@@ -9,8 +16,13 @@ const port = process.env.PORT || 3000
  */
 app.use('/api', require('./api'))
 
-app.use('/', (req, res) => {
-	res.send('Hello World')
+/**
+ * @description Esse handler será executado se nenhum outro path for encontrado
+ * na pasta pública ou nos handlers manuais (como o da API)
+ */
+app.use((req, res) => {
+	/**@todo uma mensagem de 'not found' mais bonita */
+	res.status(404).send('<h1>404 - Not found</h1>')
 })
 
 app.listen(port, () => {
