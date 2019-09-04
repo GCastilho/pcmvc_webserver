332a49a6a120699f89613b81c1cad552c0255cc7
Gabriel Castilho <castilho.gabriel14@gmail.com>
2019-09-04 14:43:07 -0300

    Configurado nodemailer
    -- Configurado nodemailer para utilizar o servidor correto da fatec
    -- Corrigido bug nos textos de email

--- src/mailer/core.js
+++ src/mailer/core.js
@@ -11,25 +11,21 @@
 const nodemailer = require('nodemailer');
 
 module.exports = async function(
-		from = '"Fred Foo 👻" <foo@example.com>',	// sender address
 		to = 'bar@example.com',						// list of receivers
 		subject = 'Hello ✔',						// Subject line
 		text = 'Hello world?',						// plain text body
 		html										// html body
 	) {
-
-	// Generate test SMTP service account from ethereal.email
-	// Only needed if you don't have a real mail account for testing
-	const testAccount = await nodemailer.createTestAccount();
+	const from = '"PCMVC Server" <adm.geee@fateccampinas.com.br>'
 
 	// create reusable transporter object using the default SMTP transport
 	const transporter = nodemailer.createTransport({
-		host: 'smtp.ethereal.email',
+		host: 'mx1.hostinger.com.br',
 		port: 587,
 		secure: false, // true for 465, false for other ports
 		auth: {
-			user: testAccount.user, // generated ethereal user
-			pass: testAccount.pass // generated ethereal password
+			user: 'adm.geee@fateccampinas.com.br',
+			pass: 'fatec@campinas'
 		}
 	});
 
@@ -41,10 +37,7 @@ module.exports = async function(
 		html
 	})
 
-	console.log('Message sent: %s', info.messageId);
-	// Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-
-	// Preview only available when sending through an Ethereal account
-	console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-	// Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+	console.log(`E-mail sent to ${info.accepted.toString()}`)
+	if (info.rejected.length > 0)
+		console.log(`Failure sending to: ${info.rejected.toString()}`)
 }
--- src/mailer/index.js
+++ src/mailer/index.js
@@ -11,56 +11,56 @@ const mailer = {}
 mailer.sendTo = {
 	newProfessor: function(destination, name, api_key, validation_link) {
 		const subject = 'Bem vindo ao Projeto Comunitário de Medição do Vento de Campinas'
-		const body = `Caro ${name}, bem vindo ao PCMVC - Projeto Comunitário de Medição do Vento de Campinas
-		
-		Você está recebendo esse e-mail pois ele foi cadastrado como um professor no nosso sistema
+		const body = `Caro ${name}, bem vindo ao PCMVC - Projeto Comunitário de Medição do Vento de Campinas\n\n` +
 
-		Esta é a sua chave da API de telemetria: ${api_key}
+		'Você está recebendo esse e-mail pois ele foi cadastrado como um professor no nosso sistema\n\n' +
 
-		Use ela no arduino para poder se autenticar em nossa API e enviar dados de velocidade do vento e contribuir para o projeto
-		
-		Para concluir o cadastro e poder ter acesso ao painel de administração use o seguinte link: http://localhost:3000/validation?link=${validation_link}`
+		`Esta é a sua chave da API de telemetria: ${api_key}\n\n` +
 
-		Core(undefined, destination, subject, body)
+		'Use ela no arduino para poder se autenticar em nossa API e enviar dados de velocidade do vento e contribuir para o projeto\n\n' +
+
+		`Para concluir o cadastro e poder ter acesso ao painel de administração use o seguinte link: http://192.168.96.97/validation?link=${validation_link}`
+
+		Core(destination, subject, body)
 	},
 
 	newAluno: function(destination, name, api_key) {
 		const subject = 'Bem vindo ao Projeto Comunitário de Medição do Vento de Campinas'
-		const body = `Caro ${name}, bem vindo ao PCMVC - Projeto Comunitário de Medição do Vento de Campinas
+		const body = `Caro ${name}, bem vindo ao PCMVC - Projeto Comunitário de Medição do Vento de Campinas\n\n` +
 
-		Você está recebendo esse e-mail pois ele foi cadastrado como um aluno no nosso sistema
+		'Você está recebendo esse e-mail pois ele foi cadastrado como um aluno no nosso sistema\n\n' +
 
-		Esta é a sua chave da API de telemetria: ${api_key}
+		`Esta é a sua chave da API de telemetria: ${api_key}\n\n` +
 
-		Use ela no arduino para poder se autenticar em nossa API e enviar dados de velocidade do vento e contribuir para o projeto`
+		'Use ela no arduino para poder se autenticar em nossa API e enviar dados de velocidade do vento e contribuir para o projeto'
 
-		Core(undefined, destination, subject, body)
+		Core(destination, subject, body)
 	},
 
 	novaSenha: function(destination, name, validation_link) {
 		const subject = 'Solicitação de redefinição de senha | Projeto Comunitário de Medição do Vento de Campinas'
 
-		const body = `Caro ${name}, foi solicitada redefinição de senha para a sua conta
+		const body = `Caro ${name}, foi solicitada redefinição de senha para a sua conta\n\n` +
 		
-		Para redefinir sua senha use o seguinte link: http://localhost:3000/validation?link=${validation_link}
+		`Para redefinir sua senha use o seguinte link: http://192.168.96.97/validation?link=${validation_link}\n\n` +
 		
-		Se não foi você que pediu a redefinição de senha, fique tranquilo, pois nada será alterado e nenhuma informação está comprometida`
+		'Se não foi você que pediu a redefinição de senha, fique tranquilo, pois nada será alterado e nenhuma informação está comprometida'
 
-		Core(undefined, destination, subject, body)
+		Core(destination, subject, body)
 	},
 
 	lembreteApi: function(destination, name, api_key) {
 		const subject = 'Solicitação de lembrete de chave de API | Projeto Comunitário de Medição do Vento de Campinas'
 
-		const body = `Caro ${name}, foi solicitado lembrete da chave de API para a sua conta
+		const body = `Caro ${name}, foi solicitado lembrete da chave de API para a sua conta\n\n` +
 
-		Esta é a sua chave da API de telemetria: ${api_key}
+		`Esta é a sua chave da API de telemetria: ${api_key}\n\n`
 
-		Use ela no arduino para poder se autenticar em nossa API e enviar dados de velocidade do vento e contribuir para o projeto
+		'Use ela no arduino para poder se autenticar em nossa API e enviar dados de velocidade do vento e contribuir para o projeto\n\n'
 		
-		Se não foi você que pediu o lembrete de chave de API, fique tranquilo, pois nenhuma informação está comprometida`
+		'Se não foi você que pediu o lembrete de chave de API, fique tranquilo, pois nenhuma informação está comprometida'
 
-		Core(undefined, destination, subject, body)
+		Core(destination, subject, body)
 	}
 }
 
